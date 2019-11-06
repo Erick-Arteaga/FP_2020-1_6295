7442a257be5b213a0812c810c6c8c96b900aa42d
Erick-Arteaga <eaartemung@gmail.com>
2019-11-05 19:52:20 -0600

    Se subio la tarea 7

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 int main(int argc, char* argu[]){
     float num1,num2,resultado=1;
+//se convertira el texto a numero
     num1=atof(argu[1]);
     num2=atof(argu[2]);
+//el for servira para llegar al resultado final
     for(int i=0;i<num2; i++){
         resultado=resultado*num1;
     }
